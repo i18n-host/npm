21277113315737f207acb314e8e5813188a00d7c
i18n.host <i18n.host@gmail.com>
2025-06-11 13:16:20 +0800

    ♨

--- gist/src/mod.js
+++ gist/src/mod.js
@@ -1,1 +1,26 @@
-export default () => {};
+import { Octokit } from "@octokit/rest"
+
+export class Gist
+  constructor: (conf)->
+    @gh = new Octokit conf
+
+  get: (id)->
+    @gh.gists.get({ gist_id: id })
+
+  set: (filename, key, val)->
+    if key
+      return
+
+    files = {}
+
+    files[filename] = { content: val } 
+
+    @gh.gists.create {
+      description
+      public: true
+      files
+    }
+    // return @gh.gists.update({ gist_id: id, files: data })
+
+export default (conf)=>
+  new Gist(conf)
