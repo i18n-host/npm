4517db6a42d4b9f536704d72c585487857484bc1
i18n.host <i18n.host@gmail.com>
2025-06-13 18:16:42 +0800

    ♨

--- updist/test.sh
+++ updist/test.sh
@@ -12,4 +12,6 @@ cd $DIR
 set -x
 
 bun x cep -c src -o lib
-./lib/mod.js i18 0.2.1 ../../conf/i18/ed25519/sk /tmp/bin/i18/0.1.0/aarch64-apple-darwin
+
+./lib/warmup.js
+# ./lib/mod.js i18 0.2.1 ../../conf/i18/ed25519/sk /tmp/bin/i18/0.1.0/aarch64-apple-darwin
